4d720ea3749e2e1ccf47478c28cf63c6df18fe43
Ryan Adams <ryanadams14@gmail.com>
2021-07-30 16:01:16 -0500

    updating choices

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ function beginManager() {
                 'Add Role',
                 'View All Departments',
                 'Add Department',
-                'Update Employee',
                 'Quit'
             ]
         }])
